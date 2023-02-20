a8fef51c890f329c2951d57f3b75b0e771da5a01
huanyuzhang <949219270@qq.com>
2023-02-20 19:34:15 +0800

    在Student累中添加了sex属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,8 @@
+package com.msb;
+
+public class Person {
+    private int age;
+    private String name;
+    private double height;
+
+}
---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,7 @@
+package com.msb;
+
+public class Student {
+    private int age;
+    private String name;
+    private String sex;
+}
